ef5c1c9d14748f612a912dcc479769bf1dd5f364
Linus Lin <linuslin92@gmail.com>
2019-08-19 00:36:09 -0500

    [08-19-2019] Add contact us form and functionality

---
+++ src/components/common/status.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import global from '../../src/styles/global.module.scss';
+import styles from './status.module.scss';
+
+export default class Status extends Component {
+    render() {
+        return (
+            <div className={`${global.formrow} ${this.props.data.display === true ? styles.display : styles.hidden}`}>
+                <div className={`${styles.status} ${styles['status' + this.props.data.type]}`}>
+                    <i className={`icons-${this.props.data.type}`}></i><span className={styles.message}>{this.props.data.message}</span>
+                </div>
+            </div>
+        )
+    }
+}
--- src/components/contactus/contactus.js
+++ src/components/contactus/contactus.js
@@ -2,15 +2,52 @@ import React, { Component, Fragment } from 'react';
 import styles from './contactus.module.scss';
 import global from '../../src/styles/global.module.scss';
 import layout from '../../src/styles/layout.module.scss';
+
 import Helmet from 'react-helmet';
 import Hero from '../common/hero';
+import Status from '../common/status';
 
 export default class Contact extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            content: this.props.content
+            content: this.props.content,
+            textcount: this.props.content.form.description.maxLength,
+            status: {
+                type: 'okay',
+                display: true,
+                message: 'Please enter required fields.'
+            }
         }
+    }
+    keydown = (e) => {
+        let timeout = 0;
+        if(e.type === 'reset') {
+            timeout = 50;
+        }
+        setTimeout(() => {
+            const $count = document.querySelector('#wordcount');
+            const $ta = document.querySelector('#textarea');
+            $count.setAttribute("textnum", $ta.maxLength - $ta.textLength);
+        }, timeout)
+    }
+    formsubmit = (e) => {
+        e.preventDefault();
+        const data = new FormData(e.target);
+
+        fetch('/cgi-bin/contactus.php', {
+            method: 'POST',
+            mode: 'cors',
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: data
+        })
+        .then(data=>data.json())
+        .then((e)=>
+            console.log(e)
+        )
     }
     render() {
         const CONT = this.state.content;
@@ -23,7 +60,7 @@ export default class Contact extends Component {
                 <Hero src={ this.props.bgImg } srcmin={ this.props.bgImgMin } />
                 <div className={global.paddingcontainer}>
                     <div className={layout.flexbox}>
-                        <div className={layout.flex6}>
+                        <div className={`${layout.flex6} ${layout.contentcard}`}>
                             <div className={global.formrow}>
                                 <label className={global.label}>{CONT.address1.label}</label>
                                 <div className={global.formtext}>{CONT.address1.text}</div>
@@ -54,20 +91,71 @@ export default class Contact extends Component {
                                 }
                             </div>
                         </div>
-                        {/*<div className={layout.flex6}>
-                            <form>
-                                <div className={styles.formrow}>
-                                    <label className={global.label}>Family Name</label>
-                                    <input className={global.forminput} name="lastname" placeholder="Last Name / Family Name" required/>
+                        <div className={`${layout.flex6} ${layout.contentcard}`}>
+                            <label htmlFor="contact" className={global.label}>{CONT.form.label}</label>
+                            <form className={global.form} onReset={this.keydown} onSubmit={this.formsubmit} method="POST">
+                                <div className={global.formrow}>
+                                    <input 
+                                        minLength="5" 
+                                        type={CONT.form.name.type} 
+                                        pattern={CONT.form.name.pattern} 
+                                        className={`${global.input} ${styles.input}`} 
+                                        name={CONT.form.name.name} 
+                                        placeholder={`${CONT.form.name.label} ${CONT.form.name.required ? "- (Required)" : ""}`} 
+                                        required={CONT.form.name.required} />
                                 </div>
 
-                                <div className={styles.formrow}>
-                                    <label className={global.label}>Given Name</label>
-                                    <input className={global.forminput} name="firstname" placeholder="Last Name / Family Name" required/>
+                                <div className={global.formrow}>
+                                    <input 
+                                        minLength="10" 
+                                        type={CONT.form.phone.type} 
+                                        className={`${global.input} ${styles.input}`} 
+                                        name={CONT.form.phone.name} 
+                                        placeholder={`${CONT.form.phone.label} ${CONT.form.phone.required ? "- (Required)" : ""}`} 
+                                        required={CONT.form.phone.required} />
+                                </div>
+
+                                <div className={global.formrow}>
+                                    <input 
+                                        minLength="5" 
+                                        type={CONT.form.email.type} 
+                                        className={`${global.input} ${styles.input}`} 
+                                        name={CONT.form.email.name} 
+                                        placeholder={`${CONT.form.email.label} ${CONT.form.email.required ? "- (Required)" : ""}`} 
+                                        required={CONT.form.email.required} />
+                                </div>
+
+                                <div className={global.formrow}>
+                                    <input 
+                                        minLength="3" 
+                                        type={CONT.form.subject.type} 
+                                        className={`${global.input} ${styles.input}`} 
+                                        name={CONT.form.subject.name} 
+                                        placeholder={`${CONT.form.subject.label} ${CONT.form.subject.required ? "- (Required)" : ""}`} 
+                                        required={CONT.form.subject.required} />
+                                </div>
+
+                                <div className={global.formrow}>
+                                    <textarea 
+                                        id="textarea"
+                                        minLength="5" 
+                                        maxLength={CONT.form.description.maxLength} 
+                                        className={`${global.textarea} ${styles.textarea}`} 
+                                        name={CONT.form.description.name} 
+                                        placeholder={`${CONT.form.description.label} ${CONT.form.description.required ? "- (Required)" : ""}`} 
+                                        required={CONT.form.description.required}
+                                        onKeyDown={this.keydown} 
+                                        onChange={this.keydown} />
+                                    <div id="wordcount" className={styles.workcount} textnum={this.state.textcount}></div>
+                                </div>
+                                <div className={`${global.formrow} ${styles.flex}`}>
+                                    <button type="submit" className={`${global.button} ${styles.button}`}>{CONT.form.submit}</button>
+                                    <button type="reset" className={`${global.button} ${global.ghostlybutton} ${styles.button}`}>{CONT.form.clear}</button>
                                 </div>
                             </form>
-                        </div>*/}
+                        </div>
                     </div>
+                    <Status data={this.state.status} />
                 </div>
             </Fragment>
         )
